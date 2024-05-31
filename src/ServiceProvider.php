359b4d5d7eb1a1527abfc81d4ad1885f643e80ee
Geert-Jan Knapen <gknapen@geertjanknapen.nl>
2024-05-31 23:58:32 +0200

    TASK/SADT-1: Setupt addon boilerplate

---
+++ src/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Geertjanknapen1\StatamicAutomaticDeeplTranslations;
+
+use Statamic\Providers\AddonServiceProvider;
+
+class ServiceProvider extends AddonServiceProvider
+{
+    public function bootAddon()
+    {
+        //
+    }
+}
